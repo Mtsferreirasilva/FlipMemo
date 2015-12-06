17cea41a2deda83b7b77e44abbe73572a05bd568
mtsferreirasilva <mtsferreirasilva@gmail.com>
2015-12-06 18:14:43 -0200

    Adding inspecting time and ALL animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 // totalCards = Total of cards for the level
-// time = time for memorizing
+// intervalTime = time for memorizing - (value * 100) miliseconds
 // animation = type of animation [all, cascade, checkerboard]
 
 var flipmemo = function(){
 	return {
 		// Available levels
 		fm_levels: {
-			1: {"totalCards": 4, "time": 6, animation: "all"},
-			2: {"totalCards": 4, "time": 4, animation: "all"},
-			3: {"totalCards": 6, "time": 6, animation: "all"},
-			4: {"totalCards": 6, "time": 4, animation: "all"},
-			5: {"totalCards": 6, "time": 4, animation: "checkerboard"}
+			1: {"totalCards": 4, "intervalTime": 40, animation: "all"},
+			2: {"totalCards": 4, "intervalTime": 40, animation: "all"},
+			3: {"totalCards": 6, "intervalTime": 60, animation: "all"},
+			4: {"totalCards": 6, "intervalTime": 40, animation: "all"},
+			5: {"totalCards": 6, "intervalTime": 40, animation: "checkerboard"}
 		},
 
 		// HTML of each block
 		fm_blockHTML: '<div class="block-item"><div class="block hover" data-card-number="{card-number}"><figure class="front"><span>{number}</span></figure><figure class="back"><span>Flip Memo</span></figure></div></div>',
+
+		// It says what is the next expected number to be clicked
+		fm_nextToBeClicked: 1,
+
+		fm_currentLevel: 1,
+
+		fm_isInspecting: false,
 
 		// Shuffles an array
 		fm_shuffle: function(o){
@@ -24,6 +31,8 @@ var flipmemo = function(){
 
 		// Creates a level HTML structure based on level and difficulty
 		fm_createLevel: function(level, difficulty){
+			this.fm_currentLevel = level;
+
 			var numbers = [];
 			for(var i = 1; i <= this.fm_levels[level].totalCards; i++){
 				numbers.push(i);
@@ -36,7 +45,77 @@ var flipmemo = function(){
 			}
 
 			document.querySelector(".gameScreen .block-container").innerHTML = html_final;
+			setTimeout(function(){
+				game.fm_inspectingTime();
+			}, 1000);
 		},
+
+		fm_inspectingTime: function(){
+			var barWidth = 100;
+			this.fm_isInspecting = true;
+
+			function loadingBar(){
+				barWidth--;
+				var bar = document.querySelector(".gameScreen .progressbar .bar");
+				bar.style.width = barWidth + '%';
+				if (barWidth == 0) {
+					clearInterval(decreaseBar);
+					game.fm_isInspecting = false;
+					game.fm_blockEventListener();
+					game.fm_nextToBeClicked = 1;
+					bar.className = "bar";
+					bar.style.width = 100 + "%";
+				};
+			}
+
+			var intervalTime = this.fm_levels[this.fm_currentLevel].intervalTime;
+			var decreaseBar = setInterval(loadingBar, intervalTime);
+			this.fm_animateCards();
+		},
+
+		fm_animateCards: function(){
+			var animation = this.fm_levels[this.fm_currentLevel].animation;
+
+			if (animation == "all") {
+				var allAnimation = setInterval(function(){
+					var blocks = document.querySelectorAll(".block");
+					for (i = 0; i < blocks.length; i++) {
+						if(blocks[i].className == "block"){
+							blocks[i].className = blocks[i].className + " hover";
+						}else{
+							blocks[i].className = "block";
+						}
+					}
+					if (!game.fm_isInspecting) {
+						clearInterval(allAnimation);
+						for (i = 0; i < blocks.length; i++) {
+							blocks[i].className = "block";
+						}
+					};
+				}, 600);
+			}else if (animation == "cascade"){
+
+			}else if (animation == "checkerboard"){
+
+			};
+		},
+
+		// Creates an event listener for each new block
+		fm_blockEventListener: function(){
+			var blocks = document.querySelectorAll(".block");
+
+			for (i = 0; i < blocks.length; i++) {
+				blocks[i].addEventListener("click", function(){
+					this.getAttribute("data-card-number");
+					if (this.getAttribute("data-card-number") == game.fm_nextToBeClicked) {
+						console.log("clicou correto");
+						game.fm_nextToBeClicked++;
+					}else{
+						console.log("clicou errado, quero o " + game.fm_nextToBeClicked);
+					};
+				});
+			}
+		}
 	}
 };
 
